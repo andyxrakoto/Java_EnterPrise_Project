5fb816017d45ec2300c9a37bd6cdefe5e537f781
Andyx <127594368+andyxrakoto@users.noreply.github.com>
2023-08-29 15:37:35 +0300

    Update DepartmentsController.java

--- src/main/java/Controller/DepartmentsController.java
+++ src/main/java/Controller/DepartmentsController.java
@@ -1,2 +1,42 @@
-package Controller;public class DepartmentsController {
+package Controller;
+
+import Model.Departments;
+import Repository.DepartmentsRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentsController {
+
+    private final DepartmentsRepository departmentsRepository;
+
+    public DepartmentsController(DepartmentsRepository departmentsRepository) {
+        this.departmentsRepository = departmentsRepository;
+    }
+
+    @GetMapping
+    public List<Departments> getAllDepartments() {
+        return departmentsRepository.getAllDepartments();
+    }
+
+    @GetMapping("/{id}")
+    public Departments getDepartmentById(@PathVariable int id) {
+        return departmentsRepository.getDepartmentById(id);
+    }
+
+    @PostMapping
+    public void createDepartment(@RequestBody Departments department) {
+        departmentsRepository.createDepartment(department);
+    }
+
+    @PutMapping("/{id}")
+    public void updateDepartment(@PathVariable int id, @RequestBody Departments department) {
+        departmentsRepository.updateDepartment(id, department);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDepartment(@PathVariable int id) {
+        departmentsRepository.deleteDepartment(id);
+    }
 }
