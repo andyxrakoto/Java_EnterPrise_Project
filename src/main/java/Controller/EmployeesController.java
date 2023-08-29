f1e70e530f3e718d5d55d37a147e49920dd07169
Andyx <127594368+andyxrakoto@users.noreply.github.com>
2023-08-29 15:38:08 +0300

    Update EmployeesController.java

--- src/main/java/Controller/EmployeesController.java
+++ src/main/java/Controller/EmployeesController.java
@@ -1,3 +1,44 @@
 package Controller;
+package Controller;
 
+import Model.Employees;
+import Repository.EmployeesRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeesController {
+
+    private final EmployeesRepository employeesRepository;
+
+    public EmployeesController(EmployeesRepository employeesRepository) {
+        this.employeesRepository = employeesRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Employees> getAllEmployees(){
+        return employeesRepository.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employees getEmployeeById(@PathVariable int id) {
+        return employeesRepository.getEmployeeById(id);
+    }
+
+    @PostMapping
+    public void createEmployee(@RequestBody Employees employee) {
+        employeesRepository.createEmployee(employee);
+    }
+
+    @PutMapping("/{id}")
+    public void updateEmployee(@PathVariable int id, @RequestBody Employees employee) {
+        employeesRepository.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable int id) {
+        employeesRepository.deleteEmployee(id);
+    }
+}
 
