f7405be3c12fb0e1131e5150c7a7d57d2f5002f5
Andyx <127594368+andyxrakoto@users.noreply.github.com>
2023-08-30 23:58:11 +0300

    Update DepartmentsService.java

--- src/main/java/Service/DepartmentsService.java
+++ src/main/java/Service/DepartmentsService.java
@@ -1,2 +1,37 @@
-package Service;public class DepartmentsService {
+package Service;
+
+import Model.Departments;
+import Repository.DepartmentsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentsService {
+    private final DepartmentsRepository departmentsRepository;
+
+    public DepartmentsService(DepartmentsRepository departmentsRepository) {
+        this.departmentsRepository = departmentsRepository;
+    }
+
+    public List<Departments> findAllDepartments() {
+        return departmentsRepository.findAllDepartments();
+    }
+
+    public Departments findDepartmentById(int id) {
+        return departmentsRepository.findDepartmentById(id);
+    }
+
+    public void createDepartment(Departments department) {
+        departmentsRepository.createDepartment(department);
+    }
+
+    public void updateDepartment(int id, Departments departments) {
+        departmentsRepository.updateDepartment(id, departments);
+    }
+
+    public void deleteDepartment(int id) {
+        departmentsRepository.deleteDepartment(id);
+    }
 }
+
