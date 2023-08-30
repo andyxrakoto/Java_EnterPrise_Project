e5f8936a892ff7277c0aff11b618f9b6e75f3cbb
Andyx <127594368+andyxrakoto@users.noreply.github.com>
2023-08-30 23:57:47 +0300

    Update EmployeesService.java

--- src/main/java/Service/EmployeesService.java
+++ src/main/java/Service/EmployeesService.java
@@ -1,2 +1,38 @@
-package Service;public class EmployeesService {
+package Service;
+
+import Model.Employees;
+import Repository.EmployeesRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EmployeesService {
+
+    private final EmployeesRepository employeesRepository;
+
+    public EmployeesService(EmployeesRepository employeesRepository) {
+        this.employeesRepository = employeesRepository;
+    }
+
+    public List<Employees> getAllEmployees(){
+        return employeesRepository.findAllEmployees();
+    }
+
+    public Employees getEmployeeById(int id) {
+        return employeesRepository.findEmployeesById(id);
+    }
+
+    public void createEmployee(Employees employee) {
+        employeesRepository.createEmployee(employee);
+    }
+
+    public void updateEmployee(int id, Employees employee) {
+        employeesRepository.updateEmployee(id, employee);
+    }
+
+    public void deleteEmployee(int id) {
+        employeesRepository.deleteEmployee(id);
+    }
 }
+
+
